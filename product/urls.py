e827534910bf4f1e8771a25a8f7e4bcf6b966089
Sanghyeok Lee <shlee322@elab.kr>
2017-04-08 18:52:32 +0900

    상품 페이지 생성

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from user import views
+from product import views
 
 urlpatterns = [
-    url(r'^login$', views.login_page),
+    url(r'^products/(?P<product_id>\d+)$', views.product_detail),
 ]
+
+# http://127.0.0.1:8000/product/products/1
--- product/views.py
+++ product/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from product.models import Product
+
+
+def product_detail(request, product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist:
+        return render(request, 'product/detail_does_not_exist.html', status=404)
+
+    return render(request, 'product/detail.html', context={
+        'product': product
+    })
